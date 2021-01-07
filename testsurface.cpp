f4de11c6283df7ddc2f73e712e7b0fd8edceb8e0
cjms16 <62988228+cjms16@users.noreply.github.com>
2021-01-07 16:46:22 +0100

    Complement

--- testsurface.cpp
+++ testsurface.cpp
@@ -6,24 +6,39 @@
 #include "doctest.h"
 
 //Constructeur non teste car aucun parametre et aucune initialisation
-
+//Teste les Getters des classes associees qui heritent de la classe surface
+//Mettre à jours les valeurs brutes si MAJ
 TEST_CASE("{surface} Acces aux proprietes des differentes surfaces"){
-    //Teste les Getters des classes associees qui heritent de la classe surface
+    double vitesseN = 1;
+    double vitesseD = 1.2;
+    double vitesseM = 0.8;
+    double vitesseT = 1;
+    int couleurN = 7;
+    int couleurD = 10;
+    int couleurM = 8;
+    int couleurT = 6;
     surfaceNormale sn{};
     surfaceDure sd{};
     surfaceMolle sm{};
     surfaceTueuse st{};
-    SUBCASE("Acces a la vitesse de la surface"){
-        REQUIRE_EQ(1,sn.getVitesse());
-        REQUIRE_EQ(1.2,sd.getVitesse());
-        REQUIRE_EQ(0.8,sm.getVitesse());
-        REQUIRE_EQ(1,st.getVitesse());
-    }
-    SUBCASE("Acces a la propriete qui decide si la balle doit mourir au contact"){
+    SUBCASE("Acces aux proprietes Morte et a la vitesse de la surface NORMALE"){
+        REQUIRE_EQ(vitesseN,sn.getVitesse());
         REQUIRE_FALSE(sn.getMorte());
-        REQUIRE_FALSE(sd.getMorte());
-        REQUIRE_FALSE(sm.getMorte());
-        REQUIRE(st.getMorte());
+        REQUIRE_EQ(couleurN,sn.getCouleur());
     }
-    //Judicieux de tester getCouleur() qui renvoie la couleur de chaque surface ??...
+    SUBCASE("Acces aux proprietes Morte et a la vitesse de la surface DURE"){
+        REQUIRE_EQ(vitesseD,sd.getVitesse());
+        REQUIRE_FALSE(sd.getMorte());
+        REQUIRE_EQ(couleurD,sd.getCouleur());
+    }
+    SUBCASE("Acces aux proprietes Morte et a la vitesse de la surface MOLLE"){
+        REQUIRE_EQ(vitesseM,sm.getVitesse());
+        REQUIRE_FALSE(sm.getMorte());
+        REQUIRE_EQ(couleurM,sm.getCouleur());
+    }
+    SUBCASE("Acces aux proprietes Morte et a la vitesse de la surface TUEUSE"){
+        REQUIRE_EQ(vitesseT,st.getVitesse());
+        REQUIRE(st.getMorte());
+        REQUIRE_EQ(couleurT,st.getCouleur());
+    }
 }
